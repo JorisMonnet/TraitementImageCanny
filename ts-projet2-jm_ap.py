2e86883c943dc80e07e15dc02f0307da76ff9db9
Monnet Joris <joris.monnet@he-arc.ch>
2020-04-28 12:34:36 +0200

    Begin to check the existing functions from internet

--- ts-projet2-jm_ap.py
+++ ts-projet2-jm_ap.py
@@ -1,32 +1,137 @@
-"""from tkinter.filedialog import askopenfilename
-from tkinter.ttk import *
-from tkinter import*
-
-root = Tk() 
-root.geometry('200x100') 
-
-def openFile():
-    file.name = askopenfilename(initialdir = "/",title = "Select file",filetypes = ("all files",'*.*'))
-    if file is not None:
-        content = file.read()
-    print("réussi")
-        #return content
-
-btn = Button(root, text ='Open', command = lambda:openFile()) 
-btn.pack(side = TOP, pady = 10)
-
-mainloop()"""
-
 from tkinter import filedialog
 from tkinter import *
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+
 import numpy as np
 
-root = Tk()
-root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
-img = mpimg.imread(root.filename)
+def getFileName():
+    root = Tk()
+    return filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("png files","*.png"),("all files","*.*")))
+"""
+def gaussian_kernel(size, sigma=1):
+    size = int(size) // 2
+    x, y = np.mgrid[-size:size+1, -size:size+1]
+    normal = 1 / (2.0 * np.pi * sigma**2)
+    g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
+    return g
+
+def sobel_filters(img):
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+    
+    Ix = ndimage.filters.convolve(img, Kx)
+    Iy = ndimage.filters.convolve(img, Ky)
+    
+    G = np.hypot(Ix, Iy)
+    G = G / G.max() * 255
+    theta = np.arctan2(Iy, Ix)
+    
+    return (G, theta)
+
+def non_max_suppression(img, D):
+    M, N = img.shape
+    Z = np.zeros((M,N), dtype=np.int32)
+    angle = D * 180. / np.pi
+    angle[angle < 0] += 180
+
+    
+    for i in range(1,M-1):
+        for j in range(1,N-1):
+            try:
+                q = 255
+                r = 255
+                
+               #angle 0
+                if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                    q = img[i, j+1]
+                    r = img[i, j-1]
+                #angle pi/4
+                elif (22.5 <= angle[i,j] < 67.5):
+                    q = img[i+1, j-1]
+                    r = img[i-1, j+1]
+                #angle pi/2
+                elif (67.5 <= angle[i,j] < 112.5):
+                    q = img[i+1, j]
+                    r = img[i-1, j]
+                #angle 3pi/4
+                elif (112.5 <= angle[i,j] < 157.5):
+                    q = img[i-1, j-1]
+                    r = img[i+1, j+1]
+
+                if (img[i,j] >= q) and (img[i,j] >= r):
+                    Z[i,j] = img[i,j]
+                else:
+                    Z[i,j] = 0
+
+            except IndexError as e:
+                pass
+    
+    return Z
+def threshold(img, lowThresholdRatio=0.05, highThresholdRatio=0.09):
+    
+    highThreshold = img.max() * highThresholdRatio
+    lowThreshold = highThreshold * lowThresholdRatio
+    
+    M, N = img.shape
+    res = np.zeros((M,N), dtype=np.int32)
+    
+    weak = np.int32(25)
+    strong = np.int32(255)
+    
+    strong_i, strong_j = np.where(img >= highThreshold)
+    zeros_i, zeros_j = np.where(img < lowThreshold)
+    
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+    
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+    
+    return (res, weak, strong)
+
+def hysteresis(img, weak, strong=255):
+    M, N = img.shape  
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            if (img[i,j] == weak):
+                try:
+                    if ((img[i+1, j-1] == strong) or (img[i+1, j] == strong) or (img[i+1, j+1] == strong)
+                        or (img[i, j-1] == strong) or (img[i, j+1] == strong)
+                        or (img[i-1, j-1] == strong) or (img[i-1, j] == strong) or (img[i-1, j+1] == strong)):
+                        img[i, j] = strong
+                    else:
+                        img[i, j] = 0
+                except IndexError as e:
+                    pass
+    return img
+"""
+img = mpimg.imread(getFileName())
 if img.dtype == np.float32: # Si le résultat n'est pas un tableau d'entiers
     img = (img * 255).astype(np.uint8)
-plt.imshow(img)
+
+imgToShow = plt.imshow(img)
+red = img[:,:,2] 
+blue = img[2,:,:]
+plt.imshow(red)
+#plt.imshow(blue)
+
+import matplotlib.pyplot as plt
+
+from skimage import data
+from skimage.color import rgb2gray
+
+original = data.astronaut()
+grayscale = rgb2gray(original)
+
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+ax = axes.ravel()
+
+ax[0].imshow(original)
+ax[0].set_title("Original")
+ax[1].imshow(grayscale, cmap=plt.cm.gray)
+ax[1].set_title("Grayscale")
+
+fig.tight_layout()
+
+
 plt.show()
